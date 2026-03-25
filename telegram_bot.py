be91071b7a5179800ec9fff7f0f04a6e2ea344c9
Omar Alqassem <59409611+omaralqassem@users.noreply.github.com>
2026-03-25 20:22:14 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,10 @@
 import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 from datetime import datetime
-
 def send_signal(signal):
     try:
         url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-
         tier_label = "PREMIUM" if signal['score'] >= 2 else "FREE"
-
         message = (
             f"🚨 MEMECOIN SIGNAL ({tier_label}) 🚨\n\n"
             f"Symbol: {signal['symbol']}\n"
@@ -18,19 +15,11 @@ def send_signal(signal):
             f"Score: {signal['score']}\n"
             f"Time: {datetime.utcnow().strftime('%H:%M UTC')}"
         )
-
         payload = {
             "chat_id": TELEGRAM_CHAT_ID,
             "text": message
         }
-
         response = requests.post(url, data=payload)
-
         print(" Telegram response:", response.text)
-
     except Exception as e:
-<<<<<<< HEAD:Telegram_bot.py
         print(" Telegram exception:", e)
-=======
-        print("Telegram exception:", e)
->>>>>>> c1956ab9a790cbd54d301040b828e0d2f73604d4:telegram_bot.py
