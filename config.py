3c6cf788effb9fdea86847e867452e84f88d47e1
Omar Alqassem <59409611+omaralqassem@users.noreply.github.com>
2026-03-25 20:34:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
+import streamlit as st
 
 DEXSCREENER_URL = "https://api.dexscreener.com/latest/dex/search?q=SOL"
 
@@ -9,14 +9,10 @@ MIN_LIQUIDITY_SIGNAL = 20000
 VOLUME_SPIKE_THRESHOLD = 0.5 
 
 INTERVAL = 300  
-
 DB_NAME = "memecoins.db"
 
-TELEGRAM_BOT_TOKEN = os.getenv("8748976085:AAFVzWuYdaVk9hwUJhsPZhMRE3ZlU0zBoDw")
-TELEGRAM_CHAT_ID = os.getenv("memecoinSource")
-
-if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
-    raise ValueError(" Telegram config missing! Set TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = st.secrets["8748976085:AAFVzWuYdaVk9hwUJhsPZhMRE3ZlU0zBoDw"]
+TELEGRAM_CHAT_ID = st.secrets["memecoinSource"]
 
 AI_PREDICT_PERIODS = 6 
 AI_PREDICT_FREQ = '5min'
